776b5061bf4f668f04e32cc6dcbfc6e2f8fddff5
allanmosesfernandes <fernzallan14@gmail.com>
2025-01-19 00:11:51 +0530

    Sign in / sign up form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import { useState } from 'react';
 
 const Login = () => {
+    const [signIn, setSignIn] = useState(true);
+
+    const toggleSignInForm = () => {
+        setSignIn(!signIn);
+    };
+
     return (
-        <div className="bg-loginBg bg-cover h-screen">
-        </div>
+        <>
+            <div className="bg-loginBg bg-cover h-screen bg-black/10 absolute w-screen -z-1"></div>
+            {/* Login Form */}
+
+            <div className="relative flex justify-center items-center h-screen">
+                <form
+                    className="bg-opacity-70 bg-black p-10 flex flex-col gap-6 w-3/12 justify-center"
+                    action=""
+                >
+                    <h2 className="text-white text-3xl font-bold">
+                        {signIn ? 'Sign In' : 'Sign Up'}
+                    </h2>
+                    {!signIn && (
+                        <input
+                            type="text"
+                            placeholder="Full name"
+                            className="p-2 bg-black/20 text-white rounded-md border border-white"
+                        />
+                    )}
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="p-2 bg-black/20 text-white rounded-md border border-white"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="p-2 bg-black/20 rounded-md border border-white"
+                    />
+                    <button className="p-2 bg-red-500 text-white rounded-md border border-red-500 font-bold">
+                        {signIn ? 'Sign in' : 'Sign up'}
+                    </button>
+                    <p
+                        className="text-white text-center min-h-20 cursor-pointer"
+                        onClick={toggleSignInForm}
+                    >
+                        {signIn
+                            ? 'New to Netflix? '
+                            : 'Already have an account? '}
+                        <span className="font-bold">
+                            Sign {signIn ? 'up' : 'in'} now
+                        </span>
+                        .
+                    </p>
+                </form>
+            </div>
+        </>
     );
 };
 
